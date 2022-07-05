42d46d8cb38ef7110d62a4ba3d128beb0548a1fc
pgomez-a <pablogomezalvarez02@gmail.com>
2022-07-05 20:14:49 +0200

    Update MultilayerPerceptron - Add Input Layer

--- lab/MultilayerPerceptron.py
+++ lab/MultilayerPerceptron.py
@@ -5,21 +5,26 @@ class MultilayerPerceptron(object):
     """
     Multilayer perceptron architecture.
     """
-    def __init__(self, weights):
+    def __init__(self, input_len, weights):
         """
         Initializes a multilayer-perceptron model.
         """
+        if not type(input_len) == int or input_len <= 0:
+            raise Exception("input_len must be a numerical positive value.")
         if not type(weights) == list or len(weights) == 0:
             raise Exception("weights must be a list of non-empty numpy ndarray of dimension 2.")
         for elem in weights:
             if not type(elem) == np.ndarray or elem.size == 0 or elem.ndim != 2:
                 raise Exception("weights must be a non-empty numpy ndarray of dimension 3.")
-        self.__size = len(weights)
+        self.__size = 1 + len(weights)
         self.__layers = np.zeros(self.__size, dtype = object)
         for pos in range(self.__size):
-            self.__layers[pos] = LayerPerceptron(weights[pos])
-            if pos != 0 and self.__layers[pos].get_dendrites() != self.__layers[pos - 1].get_neurons() + 1:
-                raise Exception("The dendrites of a neuron must be the same as the number of neurons of the previous layer.")
+            if pos == 0:
+                self.__layers[pos] = LayerPerceptron(np.ones((input_len, 1)))
+            else:
+                self.__layers[pos] = LayerPerceptron(weights[pos - 1])
+                if self.__layers[pos].get_dendrites() != self.__layers[pos - 1].get_neurons() + 1:
+                    raise Exception("The dendrites of a neuron must be the same as the number of neurons of the previous layer.")
         return
 
     def __str__(self):
@@ -27,8 +32,13 @@ class MultilayerPerceptron(object):
         Describes the structure of the multilayer perceptron.
         """
         msg = "Multilayer Perceptron has {} layers where:\n".format(self.__size)
+        pos = 0
         for layer in self.__layers:
-            msg += "\tLayer Perceptron has {} neurons where:\n".format(layer.get_neurons())
+            if pos == 0:
+                msg += "\tInput Layer Perceptron has {} neurons where:\n".format(layer.get_neurons())
+                pos += 1
+            else:
+                msg += "\tHidden Layer Perceptron has {} neurons where:\n".format(layer.get_neurons())
             for neuron in layer.get_perceptrons():
                 msg += "\t\tPerceptron has {} dendrites with weights: {}\n".format(neuron.get_dendrites(), neuron.get_weights())
         return msg
